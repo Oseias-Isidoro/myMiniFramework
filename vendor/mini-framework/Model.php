5027c507731c2b59768588f0737c30639b4dba19
Oseias Isisdoro <oseiasisidoro@gmail.com>
2022-06-19 18:43:01 -0300

    add methods update and delete to Model.php

--- vendor/mini-framework/Model.php
+++ vendor/mini-framework/Model.php
@@ -8,6 +8,9 @@ require_once __DIR__.'/DB/DB.php';
  */
 abstract class Model extends DB
 {
+    /**
+     *
+     */
     public function __construct()
     {
         parent::__construct();
@@ -32,5 +35,32 @@ abstract class Model extends DB
             "select * from $this->table where id = :id",
             [':id' => $id]
         );
+    }
+
+    /**
+     * @param int $id
+     * @param array $data
+     * @return array|false
+     */
+    public function update(int $id, array $data)
+    {
+        $attributes = '';
+
+        foreach ($data as $key => $value)
+            $attributes = $attributes.$key." = '$value'".(end($data) == $value ? '': ',');
+
+        return $this->execute(
+            "UPDATE $this->table SET $attributes WHERE id = :id;",
+            [':id' => $id]
+        );
+    }
+
+    /**
+     * @param int $id
+     * @return array|false
+     */
+    public function delete(int $id)
+    {
+        return $this->execute("DELETE FROM $this->table WHERE id = :id", [':id' => $id]);
     }
 }
